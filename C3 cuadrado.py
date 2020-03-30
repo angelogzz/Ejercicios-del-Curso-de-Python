f4d9f7e1ea065bc078ba8fd5c421c1da5bc6d6c1
angelogzz <angelogzz87@gmail.com>
2020-03-29 18:03:40 -0600

    Ejercicios del Curso de Python

---
+++ C3 cuadrado.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+angel =turtle.Turtle()
+angel.forward(50)
+angel.left(90)
+angel.forward(50)
+angel.left(90)
+angel.forward(50)
+angel.left(90)
+angel.forward(50)
+angel.left(90)
+
+turtle.mainloop()
---
+++ tur.py
@@ -0,0 +1,27 @@
+
+
+import turtle
+
+
+def main():
+    window = turtle.Screen()
+    dave = turtle.Turtle()
+
+    make_square(dave)
+
+
+    turtle.mainloop()
+
+def make_square(dave):
+    length = int(input('Tamaño de cuadrados: '))
+    make_line_and_turn(dave, 100)
+
+    for i in range(4):
+        make_line_and_turn(dave, length)
+
+def make_line_and_turn(dave, length):
+    dave.forward(length)
+    dave.left(90)
+
+if __name__ == '__main__':
+    main()
